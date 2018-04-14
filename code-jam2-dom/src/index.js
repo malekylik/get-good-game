e990d6ef8f11ce3e08b777ab5fcf7c0d58e0d03d
Maxim Kalinovskiy <malekylik@gmail.com>
2018-04-14 22:20:05 +0300

    code-jam2-dom/feature: impliment notification component

---
+++ code-jam2-dom/src/index.js
@@ -0,0 +1,90 @@
+class NotificationRenderComponent {
+    constructor(notification) {
+        const element = document.createElement('div');
+        element.classList.add('notification');
+
+        const exitButton = document.createElement('button');
+        exitButton.classList.add('notification__exit-button');
+        exitButton.innerText = 'X';
+
+        const notificationP = document.createElement('p');
+        notificationP.classList.add('notification__text');
+        notificationP.innerText = notification;
+
+        const inputs = document.createElement('div');
+        inputs.classList.add('notification__inputs');
+
+        const checkBoxContainer = document.createElement('p');
+        const checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.id = 'notification__disable-checkbox';
+
+        const checkBoxLabel = document.createElement('label');
+        checkBoxLabel.htmlFor = 'notification__disable-checkbox';
+        checkBoxLabel.innerText = 'Disable Tips';
+
+        const galleryControls = document.createElement('div');
+        inputs.classList.add('notification__gallery-controls');
+        
+        const galleryPrev = document.createElement('input');
+        galleryPrev.type = 'button';
+        galleryPrev.value = '<';
+
+        const galleryNext = document.createElement('input');
+        galleryNext.type = 'button';
+        galleryNext.value = '>';
+
+        const galleryRadiosContainer = document.createElement('div');
+        galleryRadiosContainer.classList.add('notification__gallery-radio-buttons');
+
+        for (let i = 0; i < 4; i++) {
+            const galleryRadio = document.createElement('input');
+            galleryRadio.type = 'radio';
+            galleryRadio.name = 'notification';
+
+            if (i === 0) {
+                galleryRadio.checked = true;
+            }
+
+            galleryRadiosContainer.appendChild(galleryRadio)
+
+        }
+
+        checkBoxContainer.appendChild(checkBox);
+        checkBoxContainer.appendChild(checkBoxLabel);
+
+        galleryControls.appendChild(galleryPrev);
+        galleryControls.appendChild(galleryRadiosContainer);
+        galleryControls.appendChild(galleryNext);
+
+        inputs.appendChild(checkBoxContainer);
+        inputs.appendChild(galleryControls);
+
+        element.appendChild(exitButton);
+        element.appendChild(notificationP);
+        element.appendChild(inputs);
+
+        this.element = element;
+    }
+
+    render() {
+        document.body.appendChild(this.element);
+    }
+}
+
+
+class Notification {
+    constructor(notifications = ['']) {
+        this.notifications = notifications;
+
+        this.notificationRenderComponent = new NotificationRenderComponent(this.notifications[0]);
+    }
+
+    render() {
+        this.notificationRenderComponent.render();
+    }
+}
+
+const notification = new Notification( ['asd'] );
+
+notification.render();
