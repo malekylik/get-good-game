6026b5b667fc3ce6cc125ce23fb08eac473e8317
Maxim Kalinovskiy <malekylik@gmail.com>
2018-05-02 20:41:33 +0300

    match-match-game/feature: added start page

--- match-match-game/src/classes/classes.js
+++ match-match-game/src/classes/classes.js
@@ -1,3 +1,45 @@
+const bottomCards = [
+    './src/imgs/cards/2d.jpg',
+    './src/imgs/cards/2h.jpg',
+    './src/imgs/cards/2s.jpg',
+    './src/imgs/cards/3c.jpg',
+    './src/imgs/cards/4c.jpg',
+    './src/imgs/cards/4s.jpg',
+    './src/imgs/cards/6c.jpg',
+    './src/imgs/cards/6d.jpg',
+    './src/imgs/cards/6s.jpg',
+    './src/imgs/cards/7d.jpg',
+    './src/imgs/cards/7h.jpg',
+    './src/imgs/cards/8c.jpg',
+    './src/imgs/cards/8s.jpg',
+    './src/imgs/cards/9d.jpg',
+    './src/imgs/cards/9h.jpg',
+    './src/imgs/cards/10c.jpg',
+    './src/imgs/cards/10d.jpg',
+    './src/imgs/cards/10h.jpg',
+    './src/imgs/cards/10s.jpg',
+    './src/imgs/cards/ac.jpg',
+    './src/imgs/cards/as.jpg',
+    './src/imgs/cards/jc.jpg',
+    './src/imgs/cards/jd.jpg',
+    './src/imgs/cards/jh.jpg',
+    './src/imgs/cards/js.jpg',
+    './src/imgs/cards/kd.jpg',
+    './src/imgs/cards/qh.jpg',
+];
+
+const topCards = [
+    './src/imgs/cards/back1.jpg',
+    './src/imgs/cards/back2.jpg',
+    './src/imgs/cards/back3.jpg',
+];
+
+const difficultyNumberOfCard = [
+    5 * 2,
+    6 * 3,
+    8 * 3,
+];
+
 const createElement = (tag, style = '', value = '') => {
     const el = document.createElement(tag);
 
@@ -22,60 +64,6 @@ const createCards = (count) => {
     temp.sort((l, r) => l.rand - r.rand);
 
     return temp;    
-}
-
-
-class HTMLTimerGraphicComponent {
-    constructor(element) {
-        this.element = element;
-    }
-
-    render(timer) {
-        if (!timer.isRun) {
-            this.element.innerText = timer.start();
-        } else {
-            const time = timer.getTime();
-
-            if (this.element.innerText !== time) {
-                this.element.innerText = time;
-            }
-        }
-    }
-}
-
-class HTMLCardGraphicComponent {
-    constructor(card, top, bottom) {
-        this.topSide = top;
-        this.bottomSide = bottom;
-        this.card = card;
-
-        this.isTurning = false;
-
-        this.removeAnimation = this.removeAnimation.bind(this);
-
-        card.addEventListener('animationend', this.removeAnimation);
-    }
-
-    render() {
-        if (!this.isTurning) {
-            this.topSide.classList.add('card-side-top-turn');
-            this.bottomSide.classList.add('card-side-bottom-turn');
-
-            this.isTurning = true;
-        }
-    }
-
-    removeAnimation(cardObj) {
-        if (this.isTurning) {
-            this.topSide.classList.toggle('card-side-top-turned');
-            this.bottomSide.classList.toggle('card-side-bottom-turned');
-    
-            this.topSide.classList.remove('card-side-top-turn');
-            this.bottomSide.classList.remove('card-side-bottom-turn');
-
-            this.isTurning = false;
-        } 
-    }
 }
 
 class Timer {
@@ -145,6 +133,139 @@ class Timer {
     }
 }
 
+
+class HTMLTimerGraphicComponent {
+    constructor(element) {
+        this.element = element;
+    }
+
+    render(timer) {
+        if (!timer.isRun) {
+            this.element.innerText = timer.start();
+        } else {
+            const time = timer.getTime();
+
+            if (this.element.innerText !== time) {
+                this.element.innerText = time;
+            }
+        }
+    }
+}
+
+class HTMLCardGraphicComponent {
+    constructor(card, top, bottom) {
+        this.topSide = top;
+        this.bottomSide = bottom;
+        this.card = card;
+
+        this.isTurning = false;
+
+        this.removeAnimation = this.removeAnimation.bind(this);
+
+        card.addEventListener('animationend', this.removeAnimation);
+    }
+
+    render() {
+        if (!this.isTurning) {
+            this.topSide.classList.add('card-side-top-turn');
+            this.bottomSide.classList.add('card-side-bottom-turn');
+
+            this.isTurning = true;
+        }
+    }
+
+    removeAnimation(cardObj) {
+        if (this.isTurning) {
+            this.topSide.classList.toggle('card-side-top-turned');
+            this.bottomSide.classList.toggle('card-side-bottom-turned');
+    
+            this.topSide.classList.remove('card-side-top-turn');
+            this.bottomSide.classList.remove('card-side-bottom-turn');
+
+            this.isTurning = false;
+        } 
+    }
+}
+
+class HTMLStartPageGraphicComponent {
+    constructor() {
+        this.container = document.getElementsByClassName('start-page')[0];
+
+        this.firstNameInput = document.getElementById('first-name-input');
+        this.lastNameInput = document.getElementById('last-name-input');
+        this.emailInput = document.getElementById('email-input');
+
+        this.difficultyRadios = [];
+
+        this.difficultyRadios.push(
+            document.getElementById('low-dif'),
+            document.getElementById('med-dif'),
+            document.getElementById('hight-dif')
+        );
+
+        this.skirtRadios = [];
+
+        this.skirtRadios.push(
+            document.getElementById('skirt1'),
+            document.getElementById('skirt2'),
+            document.getElementById('skirt3')
+        );
+
+        this.submitButton = document.getElementsByClassName('start-page__submit')[0];
+    }
+
+    render() {
+        this.container.style.display = 'block';
+    }
+
+    remove() {
+        this.container.style.display = 'none';
+    }
+}
+
+
+class StartPage {
+    constructor() {
+       this.graphicComponent = new HTMLStartPageGraphicComponent();
+    }
+
+    render() {
+        this.graphicComponent.render();
+    }
+
+    remove() {
+        this.graphicComponent.remove();
+    }
+
+    getDifficulty() {
+        const difficultyRadios = this.graphicComponent.difficultyRadios;
+        let difficulty;
+
+        for (let i = 0; i < difficultyRadios.length; i++) {
+            if (difficultyRadios[i].checked) {
+                difficulty = i;
+                break;
+            }
+        }
+
+        return difficultyNumberOfCard[difficulty];
+    }
+
+    getSkirt() {
+        const skirtRadios = this.graphicComponent.skirtRadios;
+        let skirt;
+
+        for (let i = 0; i < skirtRadios.length; i++) {
+            if (skirtRadios[i].checked) {
+                skirt = i;
+                break;
+            }
+        }
+
+        return topCards[skirt];
+    }
+}
+
 class Card {
     constructor(top, bottom, value, player) {
         const [card, cardTop, cardBottom] = this.createCartElement(top, bottom);
@@ -157,7 +278,7 @@ class Card {
 
         this.isSelected = false;
         this.matched = false;
-        
+
         this.select = this.select.bind(this, player);
         card.addEventListener('click', this.select);
     }
@@ -246,32 +367,49 @@ class Player {
 }
 
 class Game {
-    constructor(timerEl) {
-        this.timer = new Timer(new HTMLTimerGraphicComponent(timerEl));
+    constructor() {
+        this.timer = new Timer(new HTMLTimerGraphicComponent(createElement('div', 'timer', '00:00:00')));
         this.cards = [];
         this.matchedCards = [];
 
-        const count = 10;
-
         this.player = new Player();
+        this.startPage = new StartPage();
+    
+        this.start = this.start.bind(this);
+        this.main = this.main.bind(this);
 
-        const randomSequence = createCards(count);
-        
-        for (let i = 0; i < count; i++) {
-            this.cards.push(new Card('playing-cards-by-david-kapah-ace-of-hearts.jpg', 'playing-cards-by-david-kapah-ace-of-spades.jpg', Math.floor(randomSequence[i].value / 2), this.player));
+        this.startPage.graphicComponent.submitButton.addEventListener('click', this.start);
+    }
+
+    start(e) {
+        e.preventDefault();
+
+        this.count = this.startPage.getDifficulty();
+        this.skirt = this.startPage.getSkirt();
+
+        const randomSequence = createCards(this.count);
+
+        for (let i = 0; i < this.count; i++) {
+            const value = Math.floor(randomSequence[i].value / 2);
+            this.cards.push(new Card(this.skirt, bottomCards[value], value, this.player));
         }
 
-        const container = document.getElementsByClassName('cards')[0];
+        this.startPage.remove();
+
         const header = document.getElementsByClassName('header')[0];
+
+        const container = document.createElement('vid');
+        container.classList.add('cards');
+        document.body.appendChild(container);
 
         this.cards.forEach((e) => {e.render(container);});
         this.timer.render(header);
-    
-        this.start = this.start.bind(this);
+
+        this.main();
     }
 
-    start() {
-        requestAnimationFrame(this.start);
+    main() {
+        requestAnimationFrame(this.main);
         
 
         this.update();
--- match-match-game/src/index.js
+++ match-match-game/src/index.js
@@ -1,8 +1,4 @@
 
+const game = new Game();
 
-const timerEl = createElement('div', 'timer', '00:00:00');
-document.body.appendChild(timerEl);
-
-const game = new Game(timerEl);
-
-game.start();
+// game.start();
