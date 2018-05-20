7e0c807b6b77dcdb97ff27152be7bee8f0dfcc93
malekylik <malekylik@gmail.com>
2018-05-20 15:42:32 +0300

    code-jam4-scoreboard/feature: added visualizing

--- code-jam4-scoreboard/src/index.js
+++ code-jam4-scoreboard/src/index.js
@@ -23,7 +23,90 @@
         });
 
        return puzzlesInfo;
+    };
 
+    const drawHeader = (puzzlesInfo = []) => {
+        const tableHeadRow = document.createElement('tr');
+
+        const tdName = document.createElement('td');
+        tdName.innerText = 'user name:';
+
+        const tdPuzzlesName = [];
+
+        puzzlesInfo.forEach(({ name }) => {
+            const tdpuzzleName = document.createElement('td');
+            tdpuzzleName.innerText = name;
+
+            tdPuzzlesName.push(tdpuzzleName);
+        });
+
+        const tdTime = document.createElement('td');
+        tdTime.innerText = 'total time:';
+
+        tableHeadRow.appendChild(tdName);
+
+        tdPuzzlesName.forEach((e) => {
+            tableHeadRow.appendChild(e);
+        });
+
+        tableHeadRow.appendChild(tdTime);
+
+        tableHead.appendChild(tableHeadRow);
+    };
+
+    const drawBody = (usersInfo = {}, puzzlesInfo = []) => {
+        const keys = Object.keys(usersInfo);
+
+        const tableRows = [];
+
+        let count = 0;
+
+        for (let i = 0; i < keys.length; i++) {
+            count += 1;
+
+            const tableBodyRow = document.createElement('tr');
+
+            const tdName = document.createElement('td');
+            tdName.innerText = `${count}: ${usersInfo[keys[i]].name}`;
+
+            const tableBodyRowPuzzles = [];
+
+            let totalAmount = 0;
+
+            puzzlesInfo.forEach((e) => {
+                const puzzleInfo = document.createElement('td');
+
+                if (e.results[keys[i]] != undefined) {
+                    puzzleInfo.innerText = e.results[keys[i]].time['$numberLong'];
+                    puzzleInfo.dataset.answer = `${e.results[keys[i]].correct}: ${e.results[keys[i]].code}`;
+                } else {
+                    puzzleInfo.innerText = e.timeLimit;
+                    puzzleInfo.dataset.answer = 'no answer';
+                }
+
+
+                totalAmount += parseInt(puzzleInfo.innerText, 10);
+
+                tableBodyRowPuzzles.push(puzzleInfo);
+            });
+
+            const tdTotalTime = document.createElement('td');
+            tdTotalTime.innerText = String(totalAmount);
+
+            tableBodyRow.appendChild(tdName);
+
+            tableBodyRowPuzzles.forEach((e) => {
+                tableBodyRow.appendChild(e);
+            });
+
+            tableBodyRow.appendChild(tdTotalTime);
+
+            tableRows.push(tableBodyRow);
+        }
+
+        tableRows.forEach((e) => {
+            tableBody.appendChild(e);
+        });
     };
 
     
@@ -33,5 +116,12 @@
 
     const usersInfo = getUserInfo(userJSON);
     const puzzlesInfo = getPuzzlesUnfo(sessionsJSON.rsschool, usersInfo);
+
+    console.log(usersInfo);
+    console.log(puzzlesInfo);
+
+    drawHeader(puzzlesInfo);
+
+    drawBody(usersInfo, puzzlesInfo);
 })();
 
