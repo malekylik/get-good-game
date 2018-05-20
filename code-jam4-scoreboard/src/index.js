26c41ac2551649f79592b0b9e30837b64e6ba81e
malekylik <malekylik@gmail.com>
2018-05-20 15:07:17 +0300

    code-jam4-scoreboard/feature: added parsing of the dumps

--- code-jam4-scoreboard/src/index.js
+++ code-jam4-scoreboard/src/index.js
@@ -1,7 +1,37 @@
-
 (async () => {
-    let userJSON = await (await fetch('/src/dumps/users.json')).json();
+    const getUserInfo = (input = []) => {
+        const usersInfo = {};
 
-    console.log(userJSON);
+        input.forEach(({ uid, displayName }) => {
+            usersInfo[uid] = {
+                name: displayName
+            };
+        });
+
+        return usersInfo;
+    };
+
+    const getPuzzlesUnfo = (input = {}, usersInfo = []) => {
+        const puzzlesInfo = [];
+
+        input.rounds.forEach((e, i) => {
+            puzzlesInfo.push({
+                name: input.puzzles[i].name,
+                timeLimit: input.puzzles[i].options.timeLimit["$numberLong"],
+                results: e.solutions,
+            });
+        });
+
+       return puzzlesInfo;
+
+    };
+
+    
+
+    let userJSON = await (await fetch('/src/dumps/users.json')).json();
+    let sessionsJSON = await (await fetch('/src/dumps/sessions.json')).json();
+
+    const usersInfo = getUserInfo(userJSON);
+    const puzzlesInfo = getPuzzlesUnfo(sessionsJSON.rsschool, usersInfo);
 })();
 
