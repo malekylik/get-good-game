394ebadf3fd4f9870a42ec7bff2d5463845c3e4c
Maxim Kalinovskiy <malekylik@gmail.com>
2018-05-31 13:08:02 +0300

    game/feature: added the canvas class

--- game/src/index.js
+++ game/src/index.js
@@ -1,27 +1,36 @@
-const context2D = canvas.getContext('2d');
-let { width, height } = canvas;
+class Canvas {
+    constructor(canvas) {
+        this.width = canvas.width;
+        this.height = canvas.height;
 
-const clearCanvas = (color = '#000000') => {
-    context2D.fillStyle = color;
-    context2D.fillRect(0, 0, width, height);
-}
+        this.htmlComponent = canvas;
+        this.context = this.htmlComponent.getContext('2d');
+    }
+
+    clear(color = '#000000') {
+        this.context.fillStyle = color;
+        this.context.fillRect(0, 0, this.width, this.height);
+    }
+
+    draw() {
+        this.clear();
+
+        this.context.fillStyle = "red";
+        this.context.fillRect(10, 10, 100, 100);
+    }
+};
 
 
 class Component {
 
 };
 
-const draw = () => {
-    clearCanvas();
-
-    context2D.fillStyle = "green";
-    context2D.fillRect(10, 10, 100, 100);
-};
+const canvas = new Canvas(document.getElementsByClassName('canvas')[0]);
 
 const main = () => {
     requestAnimationFrame(main);
 
-    draw();
+    canvas.draw();
 };
 
 main();
